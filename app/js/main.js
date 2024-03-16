2e9ea860149ba77f82ce2639810417591d4167ec
Alvydas Kriauza <senianga@yahoo.se>
2024-03-16 22:23:04 +0100

    pixel perfect done

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,68 @@
 $(function(){
+
+
+ $('.foods__categorie').on('click', function() {
+
+  $('foods__categorie').removeClass('foods__categorie--active');
+  $(this).addClass('foods__categorie--active');
+
+ });
+
+
   $('.slider').slick({
 
     dots: true,
     arrows:true,
-    /* fade: true, */
 
-    /* prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"></button>',
-    nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"></button>',
-    appendArrows: '.arrows-wrap' */
   });
+
+  $('.slider').slick({
+    prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><span class="button.slick-prev.slick-arrow"></span></button>',
+    nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><span class="button.slick-next.slick-arrow"></span></button>',
+    appendArrows: '.arrows-wrap'
+  });
+
+
+
+  $(".star").rateYo({
+    rating: 3.6,
+    starWidth: "17px",
+    normalFill: "rgba(193, 193, 193, 0.3)",
+    ratedFill: "rgb(255, 184, 0)",
+    readOnly: true,
+  });
+
+    $('.filter-price__input').ionRangeSlider(
+    {
+      type: "double",
+      min: 0,
+      max: 1000,
+      from: 200,
+      to: 500,
+      grid: true,
+      prefix: $,
+      onStart: function (data) {
+        $('.').text('data.from');
+        $('.').text('data.to');
+      },
+      onChange: function (data) {
+        $('.').text('data.from');
+        $('.').text('data.to');
+    },
+  }
+  );
+
+ /*  $('.styler__class').styler(); */
+
+
+
 });
 
 
-/* prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><span class="lnr lnr-chevron-left"></span></button>',
-    nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><span class="lnr lnr-chevron-right"></span></button>',
-    appendArrows: '.arrows-wrap' */
+
+
+
+
+
+  let mixer = mixitup('.food');
+
--- gulpfile.js
+++ gulpfile.js
@@ -63,8 +63,13 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/mixitup/dist/mixitup.min.js',
     'node_modules/slick-carousel/slick/slick.js',
-    'app/js/main.js'
+    'node_modules/rateyo/src/jquery.rateyo.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+    'app/js/main.js',
+
   ])
   .pipe(concat('main.min.js'))
   .pipe(uglify())
