36e2ccf99d95b793729460d066377f96bb3855e1
Alvydas Kriauza <senianga@yahoo.se>
2024-04-07 15:25:09 +0200

    something new

--- app/js/main.js
+++ app/js/main.js
@@ -1,46 +1,40 @@
-$(function(){
+$(function () {
+  const navBtn = document.querySelectorAll(".foods__categorie-link");
 
- const navBtn = document.querySelectorAll(".foods__categorie-link");
+  navBtn.forEach(function (item) {
+    item.addEventListener("click", function () {
+      let currentBtn = item;
 
- navBtn.forEach(function(item) {
-    item.addEventListener("click", function(){
-        let currentBtn = item;
+      navBtn.forEach(function (item) {
+        item.classList.remove("foods__categorie-link--active");
+      });
 
-        navBtn.forEach(function(item) {
-          item.classList.remove('foods__categorie-link--active');
-        });
-
-        currentBtn.classList.add('foods__categorie-link--active');
-
+      currentBtn.classList.add("foods__categorie-link--active");
     });
- });
-
- //---------------burger--------------
-
- document.querySelector('.burger').addEventListener('click', function() {
-  this.classList.toggle('active');
-  document.querySelector('.burger__wrapper').classList.toggle('active');
-  document.querySelector('.navigation').classList.toggle('open');
-});
-
-//---------------/burger--------------
-
-  $('.feedback__slider.slider').slick({
-
-    dots: true,
-    arrows:true,
-  }
-);
-
-if (window.matchMedia("(max-width: 576px)").matches) {
-  $('.partners__wraper-slider.slider').slick({
-
-    dots: true,
-    arrows:false,
-    fade: true,
   });
-};
 
+  //---------------burger--------------
+
+  document.querySelector(".burger").addEventListener("click", function () {
+    this.classList.toggle("active");
+    document.querySelector(".burger__wrapper").classList.toggle("active");
+    document.querySelector(".navigation").classList.toggle("open");
+  });
+
+  //---------------/burger--------------
+
+  $(".feedback__slider.slider").slick({
+    dots: true,
+    arrows: true,
+  });
+
+  if (window.matchMedia("(max-width: 576px)").matches) {
+    $(".partners__wraper-slider.slider").slick({
+      dots: true,
+      arrows: false,
+      fade: true,
+    });
+  }
 
   $(".star").rateYo({
     rating: 3.6,
@@ -50,37 +44,25 @@ if (window.matchMedia("(max-width: 576px)").matches) {
     readOnly: true,
   });
 
-    $('.filter-price__input').ionRangeSlider(
-    {
-      type: "double",
-      min: 0,
-      max: 1000,
-      from: 200,
-      to: 500,
-      grid: true,
-      prefix: $,
-      onStart: function (data) {
-        $('.').text('data.from');
-        $('.').text('data.to');
-      },
-      onChange: function (data) {
-        $('.').text('data.from');
-        $('.').text('data.to');
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 200,
+    to: 500,
+    grid: true,
+    prefix: $,
+    onStart: function (data) {
+      $(".").text("data.from");
+      $(".").text("data.to");
     },
-  }
-  );
+    onChange: function (data) {
+      $(".").text("data.from");
+      $(".").text("data.to");
+    },
+  });
 
- /*  $('.styler__class').styler(); */
-
-
-
+  /*  $('.styler__class').styler(); */
 });
 
-
-
-
-
-
-
-  let mixer = mixitup('.food');
-
+let mixer = mixitup(".food");
